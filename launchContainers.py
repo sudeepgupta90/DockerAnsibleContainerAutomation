e9bea08b7a2ab14032adf32a52d13485f2f0bd8c
Sudeep Gupta <sudeepgupta90@gmail.com>
2018-05-29 13:55:10 +0000

    raw codes for testing ideas

--- launchContainers.py
+++ launchContainers.py
@@ -1,4 +1,18 @@
 import docker
 
-client= docker.from_env()
-client.containers.run("ubuntu:16.04", "echo hello world")
+#client= docker.from_env()
+#client.containers.run("ubuntu:16.04", "echo hello world")
+# save container id's
+
+# use low level api to map container ip's and hostname
+
+
+from jinja2 import Template
+template= """{% for host in hosts -%}{{host.name}} ansible_host={{host.ipaddress}} ansible_user=root \n{% endfor -%}"""
+
+t= Template(template)
+#t= Template("""Hellow World! """)
+x=[{"name":"x", "ipaddress":123}, {"name":"y", "ipaddress":1234}]
+print (t.render(hosts=x))
+
+
